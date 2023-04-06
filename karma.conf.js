ee34b4f18cd642c524c8a06133cb3cf38ece09a9
Dhaiwat / dhai.eth <dhaiwatpandya@gmail.com>
2023-04-07 05:29:57 +0530

    add karma and jasmine

---
+++ karma.conf.js
@@ -0,0 +1,32 @@
+module.exports = function (config) {
+  config.set({
+    plugins: ['karma-jasmine', 'karma-chrome-launcher', 'karma-typescript'],
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine', 'karma-typescript'],
+
+    // list of files / patterns to load in the browser
+    // Here I'm including all of the the Jest tests which are all under the __tests__ directory.
+    // You may need to tweak this patter to find your test files/
+    files: ['./karma-setup.js', 'packages/**/*.test.ts'],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      // Use webpack to bundle our tests files
+      'packages/**/*.test.ts': ['karma-typescript'],
+    },
+    browsers: ['ChromeHeadless'],
+    karmaTypescriptConfig: {
+      bundlerOptions: {
+        // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
+        transforms: [require('karma-typescript-es6-transform')()],
+      },
+      tsconfig: './tsconfig.json',
+    },
+  });
+};
