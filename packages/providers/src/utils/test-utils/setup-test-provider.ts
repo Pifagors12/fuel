2942b33bb282c1172804a43fc71d4059ccf40450
nedsalk <nedim.salkic@fuel.sh>
2023-11-13 19:13:54 +0100

    refactor: consensusKey already has default

--- packages/providers/src/utils/test-utils/setup-test-provider.ts
+++ packages/providers/src/utils/test-utils/setup-test-provider.ts
@@ -33,7 +33,6 @@ export async function setupTestProvider<
 
   const nodeOptions: Partial<LaunchTestNodeOptions> = {
     ...options?.nodeOptions,
-    consensusKey: '0xa449b1ffee0e2205fa924c6740cc48b3b473aa28587df6dab12abc245d1f5298',
     chainConfig: mergeDeepRight(defaultChainConfig, options?.nodeOptions?.chainConfig || {}),
   };
 
